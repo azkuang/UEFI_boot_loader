5c269711d676c0501d266fcfbe9cbaa08fb62c3f
azkuang <alexzkuang0314@gmail.com>
2025-09-02 11:36:09 -0400

    Testing credentials

--- BootManagerPkg/Application/BootManager/console.c
+++ BootManagerPkg/Application/BootManager/console.c
@@ -4,7 +4,9 @@
 #include <Library/UefiBootServicesTableLib.h>
 
 // Initialize console protocols
-EFI_STATUS console_init(void);
+EFI_STATUS console_init(void) {
+	
+}
 
 // Print colored text to console
 void console_print_colored(CHAR16 *text, UINTN foreground, UINTN background);
